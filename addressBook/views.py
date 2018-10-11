d53c6bb10877bd1d4b2d5d4ded25d1c658424624
croolicjah <j.krawczewski@gmail.com>
2018-10-11 16:35:40 +0200

    działa dev widok Grupy, hover na przyciskach, plik z użytymi css

--- addressBook/views.py
+++ addressBook/views.py
@@ -15,35 +15,37 @@ from addressBook.models import Person, PhoneNumber, Address, EmailAddress, Group
 @method_decorator(csrf_exempt, name='dispatch')
 class ContactList(View):
 
-    def get_list(self, type=0):
+    def set_list_taste(self, type=0):
 
-        __html = ''
-        contact_list = Person.objects.order_by('surname', 'name')  # wyciągam z bazy uporządkowaną listę
+        __html = ""
+        contact_list = Person.objects.order_by("surname", "name")  # wyciągam z bazy uporządkowaną listę
 
         if type == 0:  #lista nazwisk z przyciskami z boku - edycja, usuń
             for i in contact_list:
-                __html = __html + f""" <fieldset class="position-site"><legend>Kontakty</legend>       
+                __html += f"""
                     <button class="name-surname" name="cinfo" value="{i.id}">{i.name} {i.surname}</button>
                     <div class="buttons-div">
                         <button class="button" type="submit" name="edit" value="{i.id}">Edycja</button>
                         <button class="button" type="submit" name="delete" value="{i.id}">Usuń</button></div>
                     <div class="clearfix"></div>                  
                 """
+            __html = """<fieldset class="position-site"><legend>Kontakty</legend>""" + __html
         elif type == 1:
             #contact_list = Person.objects.filter(group__member="").order_by('surname', 'name')
             for i in contact_list:
-                __html = __html + f"""<fieldset class="position-site"><legend>{i}</legend>        
+                __html += f"""      
                     <button class="name-surname" name="cinfo" value="{i.id}">{i.name} {i.surname}</button>
                     <div class="buttons-div">
                         <button class="button" type="submit" name="delete" value="{i.id}">Usuń z grupy</button></div>
                     <div class="clearfix"></div>                  
                 """
+            __html = f"""<fieldset class="position-site" ><legend>{i}</legend>""" + __html
         return __html
 
-    def show_contact_list(self, order=0):
 
+
+    def set_contact_list(self, order=0):
         self.order = order
-
         __html_start = """
             <html><head>
                   
@@ -77,6 +79,8 @@ class ContactList(View):
                               color: #555;
                               height: 30px;
                               font-size: 23px;
+                              
+
                         }
                         
                         .buttons-div {
@@ -103,9 +107,13 @@ class ContactList(View):
                             clear: both;
                         }
                         
-                       
+                        .button:hover {
+                           
+                           color: #ddd;
+                           background-color: #555;
+                        }
                         
-                        .button{
+                        .button {
                             display: inline-block;
                             *display: inline;
                             zoom: 1;
@@ -150,120 +158,8 @@ class ContactList(View):
                         }                  
                     </style>
                 </head><body>
-              <form action="#" method="POST"><div style="float: left;">
-                
+              <form action="#" method="POST"><div style="float: left;">       
         """
-
-        __html_start2 = """
-                    <html><head>
-
-                            <style>
-                                .name-surname {
-                                    float: left;
-                                    width: 250px;
-
-                                    cursor: pointer;
-                                    font-size: 22px;
-                                    font-weight: bold;
-                                    font-variant: small-caps;
-                                    color: black;
-                                    text-decoration: none
-                                     /*optional*/
-                                    font-family:arial,sans-serif; /*input has OS specific font-family*/
-                                      background:none;
-                                     border:none; 
-                                     padding:0;
-                                     text-align: left;
-
-                                }
-
-                                .name-surname:active {
-                                     color:gray;
-
-                                }
-
-                                .name-surname:hover{
-                                      background-color: #eee;
-                                      color: #555;
-                                      height: 30px;
-                                      font-size: 23px;
-                                }
-
-                                .buttons-div {
-                                    float: left;
-                                    height: 35px;
-                                    padding-left: 5px;
-                                }
-
-                                .bottom-buttons {
-                                    margin-top: 50px;
-
-                                }
-
-                                .position-site {
-                                    margin-top: 100px;
-                                    margin-left: 200px;
-                                    font-size: 20px;
-                                    font-weight: bold;
-                                    font-variant: small-caps;
-                                    padding-top: 20px;
-                                    display: inline-block;
-                                }
-                                .clearfix {
-                                    clear: both;
-                                }
-
-
-
-                                .button{
-                                    display: inline-block;
-                                    *display: inline;
-                                    zoom: 1;
-                                    padding: 6px 20px;
-                                    margin: 0;
-                                    cursor: pointer;
-                                    border: 1px solid #bbb;
-                                    overflow: visible;
-                                    font: bold 13px arial, helvetica, sans-serif;
-                                    text-decoration: none;
-                                    white-space: nowrap;
-                                    color: #555;
-
-                                    background-color: #ddd;
-                                    background-image: -webkit-gradient(linear, left top, left bottom, from(rgba(255,255,255,1)), to(rgba(255,255,255,0)));
-                                    background-image: -webkit-linear-gradient(top, rgba(255,255,255,1), rgba(255,255,255,0));
-                                    background-image: -moz-linear-gradient(top, rgba(255,255,255,1), rgba(255,255,255,0));
-                                    background-image: -ms-linear-gradient(top, rgba(255,255,255,1), rgba(255,255,255,0));
-                                    background-image: -o-linear-gradient(top, rgba(255,255,255,1), rgba(255,255,255,0));
-                                    background-image: linear-gradient(top, rgba(255,255,255,1), rgba(255,255,255,0));
-
-                                    -webkit-transition: background-color .2s ease-out;
-                                    -moz-transition: background-color .2s ease-out;
-                                    -ms-transition: background-color .2s ease-out;
-                                    -o-transition: background-color .2s ease-out;
-                                    transition: background-color .2s ease-out;
-                                    background-clip: padding-box; /* Fix bleeding */
-                                    -moz-border-radius: 3px;
-                                    -webkit-border-radius: 3px;
-                                    border-radius: 3px;
-                                    -moz-box-shadow: 0 1px 0 rgba(0, 0, 0, .3), 0 2px 2px -1px rgba(0, 0, 0, .5), 0 1px 0 rgba(255, 255, 255, .3) inset;
-                                    -webkit-box-shadow: 0 1px 0 rgba(0, 0, 0, .3), 0 2px 2px -1px rgba(0, 0, 0, .5), 0 1px 0 rgba(255, 255, 255, .3) inset;
-                                    box-shadow: 0 1px 0 rgba(0, 0, 0, .3), 0 2px 2px -1px rgba(0, 0, 0, .5), 0 1px 0 rgba(255, 255, 255, .3) inset;
-                                    text-shadow: 0 1px 0 rgba(255,255,255, .9);
-
-                                    -webkit-touch-callout: none;
-                                    -webkit-user-select: none;
-                                    -khtml-user-select: none;
-                                    -moz-user-select: none;
-                                    -ms-user-select: none;
-                                    user-select: none;
-                                }                  
-                            </style>
-                        </head><body>
-                      <form action="#" method="POST">
-                        <fieldset class="position-site"><legend>Kontakty</legend>
-                """
-
         __html_end = """
                   <div class=bottom-buttons>
                     <button class="button" type="submit" name="event" value="add">
@@ -275,25 +171,11 @@ class ContactList(View):
               </form>
             </body></html>
         """
-
-        __html = ''
-        if self.order == 0:
-            contact_list = self.get_list()
-            for i in contact_list:
-                __html += i
-
-            return __html_start,__html, __html_end
-        elif self. order == 1:
-
-            return __html_start, __html, __html_end
-        elif order == 2: pass
+        __html = self.set_list_taste(self.order)
+        return " ".join((__html_start, __html, __html_end))
 
     def get(self, request):
-        response = ""
-        __html = self.show_contact_list()
-        for i in __html:
-            response += i
-        return HttpResponse(response)
+        return HttpResponse(self.set_contact_list())
 
 
     def post(self, request):
@@ -302,51 +184,11 @@ class ContactList(View):
         elif self.request.POST.get('delete'):
             __delete = self.request.POST.get('delete')
             confirm_box = f"""
+                
                 <form action="" method="post">
                   <div style='margin-top: 100px; margin-left: 200px;'>
                      <h3 style='font-familly: arial, san-serif; font-size: 22px; font-variant: small-caps;'>Na pewno chcesz skasować?</h3>
                     <input type="hidden" id="deleteId" name="deleteId" value="{__delete}">
-                    <input style='
-                            display: inline-block;                      
-                            zoom: 1;
-                            padding: 6px 20px;
-                            margin: 0;
-                            cursor: pointer;
-                            border: 1px solid #bbb;
-                            overflow: visible;
-                            font: bold 13px arial, helvetica, sans-serif;
-                            text-decoration: none;
-                            white-space: nowrap;
-                            color: #555;
-                            
-                            background-color: #ddd;
-                            background-image: -webkit-gradient(linear, left top, left bottom, from(rgba(255,255,255,1)), to(rgba(255,255,255,0)));
-                            background-image: -webkit-linear-gradient(top, rgba(255,255,255,1), rgba(255,255,255,0));
-                            background-image: -moz-linear-gradient(top, rgba(255,255,255,1), rgba(255,255,255,0));
-                            background-image: -ms-linear-gradient(top, rgba(255,255,255,1), rgba(255,255,255,0));
-                            background-image: -o-linear-gradient(top, rgba(255,255,255,1), rgba(255,255,255,0));
-                            background-image: linear-gradient(top, rgba(255,255,255,1), rgba(255,255,255,0));
-                            
-                            -webkit-transition: background-color .2s ease-out;
-                            -moz-transition: background-color .2s ease-out;
-                            -ms-transition: background-color .2s ease-out;
-                            -o-transition: background-color .2s ease-out;
-                            transition: background-color .2s ease-out;
-                            background-clip: padding-box; /* Fix bleeding */
-                            -moz-border-radius: 3px;
-                            -webkit-border-radius: 3px;
-                            border-radius: 3px;
-                            -moz-box-shadow: 0 1px 0 rgba(0, 0, 0, .3), 0 2px 2px -1px rgba(0, 0, 0, .5), 0 1px 0 rgba(255, 255, 255, .3) inset;
-                            -webkit-box-shadow: 0 1px 0 rgba(0, 0, 0, .3), 0 2px 2px -1px rgba(0, 0, 0, .5), 0 1px 0 rgba(255, 255, 255, .3) inset;
-                            box-shadow: 0 1px 0 rgba(0, 0, 0, .3), 0 2px 2px -1px rgba(0, 0, 0, .5), 0 1px 0 rgba(255, 255, 255, .3) inset;
-                            text-shadow: 0 1px 0 rgba(255,255,255, .9);
-                            
-                            -webkit-touch-callout: none;
-                            -webkit-user-select: none;
-                            -khtml-user-select: none;
-                            -moz-user-select: none;
-                            -ms-user-select: none;
-                            user-select: none;' type="submit" name="confirmation" value="Potwierdź">
                     <button style='
                             display: inline-block;                      
                             zoom: 1;
@@ -387,12 +229,59 @@ class ContactList(View):
                             -khtml-user-select: none;
                             -moz-user-select: none;
                             -ms-user-select: none;
-                            user-select: none;' onclick="window.history.back();" type="button">Wróć</button>
+                            user-select: none;' onmouseover="this.style.backgroundColor = '#555'"; 
+                            onmouseout="this.style.backgroundColor = '#ddd';" type="submit" name="confirmation" 
+                            value="yes">Potwierdź</button>                    
+                    
+                    <button style='
+                            display: inline-block;                      
+                            zoom: 1;
+                            padding: 6px 20px;
+                            margin: 0;
+                            cursor: pointer;
+                            border: 1px solid #bbb;
+                            overflow: visible;
+                            font: bold 13px arial, helvetica, sans-serif;
+                            text-decoration: none;
+                            white-space: nowrap;
+                            color: #555;
+                            
+                            background-color: #ddd;
+                            background-image: -webkit-gradient(linear, left top, left bottom, from(rgba(255,255,255,1)), to(rgba(255,255,255,0)));
+                            background-image: -webkit-linear-gradient(top, rgba(255,255,255,1), rgba(255,255,255,0));
+                            background-image: -moz-linear-gradient(top, rgba(255,255,255,1), rgba(255,255,255,0));
+                            background-image: -ms-linear-gradient(top, rgba(255,255,255,1), rgba(255,255,255,0));
+                            background-image: -o-linear-gradient(top, rgba(255,255,255,1), rgba(255,255,255,0));
+                            background-image: linear-gradient(top, rgba(255,255,255,1), rgba(255,255,255,0));
+                            
+                            -webkit-transition: background-color .2s ease-out;
+                            -moz-transition: background-color .2s ease-out;
+                            -ms-transition: background-color .2s ease-out;
+                            -o-transition: background-color .2s ease-out;
+                            transition: background-color .2s ease-out;
+                            background-clip: padding-box; /* Fix bleeding */
+                            -moz-border-radius: 3px;
+                            -webkit-border-radius: 3px;
+                            border-radius: 3px;
+                            -moz-box-shadow: 0 1px 0 rgba(0, 0, 0, .3), 0 2px 2px -1px rgba(0, 0, 0, .5), 0 1px 0 rgba(255, 255, 255, .3) inset;
+                            -webkit-box-shadow: 0 1px 0 rgba(0, 0, 0, .3), 0 2px 2px -1px rgba(0, 0, 0, .5), 0 1px 0 rgba(255, 255, 255, .3) inset;
+                            box-shadow: 0 1px 0 rgba(0, 0, 0, .3), 0 2px 2px -1px rgba(0, 0, 0, .5), 0 1px 0 rgba(255, 255, 255, .3) inset;
+                            text-shadow: 0 1px 0 rgba(255,255,255, .9);
+                            
+                            -webkit-touch-callout: none;
+                            -webkit-user-select: none;
+                            -khtml-user-select: none;
+                            -moz-user-select: none;
+                            -ms-user-select: none;
+                            user-select: none;' onmouseover="this.style.backgroundColor = '#555'"; 
+                            onmouseout="this.style.backgroundColor = '#ddd'; this.color = '#555';" onclick="window.history.back();" 
+                            type="button">Wróć</button>
+ 
                 </form></body><html>
                 </div>
             """
             return HttpResponse(confirm_box)
-        elif self.request.POST.get('confirmation') == "Potwierdź":
+        elif self.request.POST.get('confirmation') == "yes":
             __delete = self.request.POST.get("deleteId")
             Person.objects.get(pk=__delete).delete()
             return HttpResponse(self.show_contact_list())
@@ -404,25 +293,18 @@ class ContactList(View):
             return HttpResponse("/edit")
 
 
-#@method_decorator(csrf_exempt, name='dispatch')
+@method_decorator(csrf_exempt, name='dispatch')
 class ContactGroupsList(ContactList):
 
-
     def get(self, request):
-        __html = self.show_contact_list()
-        response = ""
-
-
-        __html_start = self
+        __html = ''
         for i in range(6):
-            if i % 3 == 0:
-                pass
-
-
-        return HttpResponse("")
+            __html += self.set_contact_list(1)
+        return HttpResponse(__html)
 
     def post(self, request):
-         return HttpResponse("Czekam na Widok Dodawania Rekordu.")
+        return HttpResponse("Czekam na Widok")
+
 
 @method_decorator(csrf_exempt, name='dispatch')
 class EditContact(View):
