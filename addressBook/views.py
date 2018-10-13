39f88b57a639297ef8d8dd7dd4734b9c00007653
croolicjah <j.krawczewski@gmail.com>
2018-10-13 20:30:45 +0200

    widok edycji ładuje wszystkie potrzebne dane. brak obsługi przycisków

--- addressBook/views.py
+++ addressBook/views.py
@@ -122,7 +122,7 @@ class ContactList(View):
                     Grupy</button>
                 </div>"""
         elif type_view == 1:
-            __group = Group.objects.get(pk=group_view)  # wyciągam z bazy  listę wszystkie wpisy dla danej grupy
+            __group = Group.objects.get(pk=group_view)  # wyciągam z bazy  listę wszystkie wpisy dla danej grupy (group_view 1-6)
             group_list = __group.member.all()  # tworzę listę osób przypisanych do grupy
 
             for i in group_list:
@@ -136,7 +136,7 @@ class ContactList(View):
                      + __html
         return __html
 
-    def set_contact_list(self, group=1, type_view=0):
+    def set_contact_list_view(self, group=1, type_view=0):
         self.type_view = type_view
         self.group_view = group
         __html_start = """
@@ -262,7 +262,7 @@ class ContactList(View):
         return " ".join((__html_start, __html, __html_end))
 
     def get(self, request):
-        return HttpResponse(self.set_contact_list())
+        return HttpResponse(self.set_contact_list_view())
 
     def post(self, request):
         __id = self.request.POST.get('edit')
@@ -299,7 +299,7 @@ class ContactGroupsList(ContactList):
         __html = ''
         __group_list = Group.objects.all().order_by('name')
         for i in __group_list:
-            __html += self.set_contact_list(i.id, 1)
+            __html += self.set_contact_list_view(i.id, 1)
         return HttpResponse(__html)
 
     def post(self, request):
@@ -319,68 +319,96 @@ class EditContact(View):
     #        return item
     #     elif type == 0 and item :
 
+    # metoda zmienia wybrany klucz QuerrySet w listę. Np. nazwy grup wyciąga ze słowników Seta
+    # i tworzy listę z przypisanymi nazwami. patrz utworzenie listy groups w set_contact_info
+    @staticmethod
+    def convert_queryfield_to_list(querry, field):
+        __field_list = []
+        __result = [entry for entry in querry]
+        [__field_list.append(i[field]) for i in __result]
+        return __field_list
 
     # metoda zwróci tuplę z całe info z jednego wpisu w  addressBook
     def set_contact_info(self, id):
         self.__contact = Person.objects.get(pk=id)
         __contact_set = {}
-        if id != 0:
-            member_list =  (item for item in self.__contact.group_set.values()[0])
-            print(member_list)
+
+        # tworzę stringa do grup, to do wersji pierwszej. potem wstawię multiselect boxa i nie będzie potrzebne
+
+        user_groups = self.__contact.group_set.values()
+        groups = self.convert_queryfield_to_list(user_groups, 'name')
+
+
+        print(groups)
+        if id != 0:  # set of data fo edit view
+
+
             __contact_set = {
-            # atrybuty modelu Person:
+                # atrybuty modelu Person:
                 'name': self.__contact.name,
                 'surname': self.__contact.surname,
                 'description': self.__contact.description,
-             # atrybuty modelu PhoneNumber (tupla):
-                'phone': (self.__contact.phonenumber_set.values()[0]['number'],
-                         self.__contact.phonenumber_set.values()[0]['category']),
-            # atrybuty modelu EmailAddress:
-                'email': (self.__contact.emailaddress_set.values()[0]['address'],
-                          self.__contact.emailaddress_set.values()[0]['category']),
-            # atrybuty modelu Address:
-               'address': (self.__contact.address_set.values()[0]['street'],
-                           self.__contact.address_set.values()[0]['house_no'],
-                           self.__contact.address_set.values()[0]['flat_no'],
-                           self.__contact.address_set.values()[0]['city']),
-                'groups': (self.__contact.group_set.values()[0]['name']),
-
-                'taste': "EDIT"
-            }
-        elif id == 1:  # new record
-            __contact_set = {
-            # set na potrzeby okna dodawania wpisu do addressBook
-                'name': "Wpisz imię",
-                'surname': "Wpisz nazwisko",
-                'description': "Podaj opis",
                 # atrybuty modelu PhoneNumber (tupla):
-                'phone': self.__contact.phonenumber_set.values()[0]['name'],
+                'phone': (self.__contact.phonenumber_set.values()[0]['number'],  # dane telefonu w jednej tupli
+                          self.__contact.phonenumber_set.values()[0]['category']),
                 # atrybuty modelu EmailAddress:
-                'email': (self.__contact.emailaddress_set.values()[0]['address'],
+                'email': (self.__contact.emailaddress_set.values()[0]['address'],  # dane e-mail w jednej tupli
                           self.__contact.emailaddress_set.values()[0]['category']),
                 # atrybuty modelu Address:
-                'address': (self.__contact.address_set.values()[0]['street'],
+                'address': (self.__contact.address_set.values()[0]['street'],  # adres w jednej tupli
                             self.__contact.address_set.values()[0]['house_no'],
                             self.__contact.address_set.values()[0]['flat_no'],
-                            self.__contact.address_set.values()[0]['city']),
-                'taste': "NEW"
+                            "02-202", self.__contact.address_set.values()[0]['city']),
+                            # dodałem kod pocztowy, nieobsługiwany w bazie
+                'groups': groups,
+
+                'taste': "EDIT"
+                # potrzebuję tej zmiennej, by zarządzać widokami (dodawanie, edycja, wyświetlanie)
+                # przy pomocy jednej html-ki. podzielony łańcuch na __html_start, __html__end
+                # w set_contact_edit_view pozwoli mi zarządzać także buttonami w zależności od potrzeb
             }
+
         elif id == 2: # tryb info
-            __contact_set['taste'] = 'INFO'
+            __contact_set['taste'] = 'INFO'  # poza buttonami 'zapisz', 'wróć' widok INFO będzie
+            # taki sam jak widoku edit. inputy będą 'readonly'. imho będzie łatwiej i spójniej
+            # ux-owo z innymi komponentami aplikacji
+
         return __contact_set
 
-    def set_contact_edit(self, id):
-        __if_selected = ''
-        __contact_info = self.set_contact_info(id)  # pobieram całe info o kontakcie.
-        # przygotowuję html od lekkiego ostylowania. w kodzie html 'zaszyję' fragmenty ze stylami inline
+    def set_contact_edit_view(self, id, type_view=0):  # id - by wyciągnąć konkretny kontakt, type_view pozwoli dobrać zestaw w zalezności od widoku
+
+        """
+        najpierw tworzę listę pustych łańcuchów. dzięki nim rozpoznam stany kategorii e-mail oraz telefonu. jeden
+        element podmienię na 'selected' - co sprawi że pojawi się kategoria ustalona przez użytkownika w obu menu. kategorii nie ustawiam dla widoku dodawania wpisu.
+        """
+        __if_selected = ['']*8  # 8 slotów
+        __contact_info = self.set_contact_info(id)  # teraz pobieram całe info o kontakcie. trzymam je w dictionary
+
+        __if_selected[int(__contact_info['phone'][1]) - 1] = "selected"
+        # lista kategorii telefonu wyświetli się z wybranym widocznym - będzie na 1. miejscu. daję -1,
+        # bo indeksy w html-ce startują od zera, a kategorie w bazie oznaczone są od 1-4
+
+        __if_selected[int(__contact_info['email'][1]) + 3] = "selected"
+        # dokładnie jak wyżej, ale dla e-mail.. łańcuchy z listy __if_selected trafią do html-ki poniżej.
+        # to nie  daję -1, a +3, bo łańcuchy w html-ce mają indeksy 4,5,6,7 (w __if_selected),
+        # a kategorie to indeksy (dokładnie tak jak w phone) 1, 2, 3, 4. dlatego trzeba dodać 3 i zająć
+        # pozycje od 4 do 7
+
+        groups_string = ', '.join(__contact_info['groups'])
+        # tworzę tu stringa, którego na razie wrzucę w formularz Grupy
+
         input_style = '<input style="height: 33px;'
         label_style = '<label style="height: 33px; font-size: 22px; font-weight: bold; font-variant: small-caps;'
+        # przygotowuję html od lekkiego ostylowania. w html-ce 'zaszyję' fragmenty stringa stylami inline
+        # to tylko ostylowanie, nie wpłynie na działanie programu. podmieniam jedynie stringi
 
+        # poniżej html-ka z różnymi {}. dzięki atrybutom z metody set_contact_info, podmieniam części łańcuchów, np.
+        #  na 'selected' w rozwijalnych listach lub 'readonly' dla inputów w widoku INFO.
         __html_start = f"""
             <html><body><form style="margin-top: 100px; margin-left: 200px" action="/Edit/{id}" method="POST">
                 <p><label {label_style}">
                   Imię:
-                  {input_style} width: 130px" type="text" name="user_name" value="{__contact_info['name']}">
+                  {input_style} width: 130px" type="text" name="user_name" value="{__contact_info['name']}" >
                   Nazwisko:
                   {input_style} width: 250px" type="text" name="user_surname" value="{__contact_info['surname']}">
                 </label></p>   
@@ -391,15 +419,15 @@ class EditContact(View):
                 </label></p>          
                 <p><label {label_style}">Telefon:
                   {input_style} width: 295px;" type="tel" name="phone_number" value="{__contact_info['phone'][0]}" 
-                  pattern="(\+(\d{1})*\d{1})*\s*\d{2,3}\s*\d{2,3}\s*\d{2,3}\s*\d{2,3}">
+                  pattern='(\+(\d{1})*\d{1})*\s*\d{2,3}\s*\d{2,3}\s*\d{2,3}\s*\d{2,3}'>
                 </label>
                 <select style='width: 150px; padding: 6px 20px; margin: 0; border: 1px solid #bbb;
                     overflow: visible; font: 13px arial, helvetica, sans-serif;
                     text-decoration: none; white-space: nowrap; color: #555;'>
-                  <option value='private' {__if_selected}>{CATEGORIES[0][1]}</option>
-                  <option value='home' {__if_selected}>{CATEGORIES[1][1]}</option>
-                  <option value='work' {__if_selected}>{CATEGORIES[2][1]}</option>
-                  <option value='secret' {__if_selected}>{CATEGORIES[3][1]}</option>
+                  <option value='private' name="phone_cat_1" {__if_selected[0]}>{CATEGORIES[0][1]}</option>
+                  <option value='home' name="phone_cat_2" {__if_selected[1]}>{CATEGORIES[1][1]}</option>
+                  <option value='work' name="ephone_cat_3" {__if_selected[2]}>{CATEGORIES[2][1]}</option>
+                  <option value='secret' name="phone_cat_4" {__if_selected[3]}>{CATEGORIES[3][1]}</option>
                 </select></p>         
                 <p><label {label_style}">E-mail:
                   {input_style} width: 310px;" type="email" name="user_email" value="{__contact_info['email'][0]}">
@@ -407,10 +435,10 @@ class EditContact(View):
                 <select style='width: 150px; padding: 6px 20px; margin: 0; border: 1px solid #bbb;
                     overflow: visible; font: 13px arial, helvetica, sans-serif;
                     text-decoration: none; white-space: nowrap; color: #555;'>
-                  <option value="private" {__if_selected}>{CATEGORIES[0][1]}</option>
-                  <option value="home" {__if_selected}>{CATEGORIES[1][1]}</option>
-                  <option value="work"{__if_selected}>{CATEGORIES[2][1]}</option>
-                  <option value="secret" {__if_selected}>{CATEGORIES[3][1]}</option>
+                  <option value="private" name="email_cat_1" {__if_selected[4]}>{CATEGORIES[0][1]}</option>
+                  <option value="home" name="email_cat_2" {__if_selected[5]}>{CATEGORIES[1][1]}</option>
+                  <option value="work" name="email_cat_3" {__if_selected[6]}>{CATEGORIES[2][1]}</option>
+                  <option value="secret" name="email_cat_4" {__if_selected[7]}>{CATEGORIES[3][1]}</option>
                 </select></p>                
                 <fieldset style="width: 510px;"><legend style="font-size: 22px; font-weight: bold; font-variant: small-caps;">Adres:</legend>
                      <p><label {label_style}">
@@ -427,23 +455,20 @@ class EditContact(View):
                       {input_style} width: 292px;" type="text" name="city" value="{__contact_info['address'][3]}">     
                 </fieldset>                        
          """
-
         __html_group = f"""
           <p><label {label_style}">
               Grupy:
-              {input_style} width: 465px;" type="text" name="user_group" value=", ".join(me)>
+              {input_style} width: 465px;" type="text" name="user_group" value="{groups_string}">
           </label> </p>       
         """
-
         __html_end = f"""<p>{ContactList.button_save}</p></form></body></html>
         """
 
-
-        return __html_start + __html_group + __html_end
+        return __html_start + __html_group + __html_end  # przekazujemy gotowy html
 
     def get(self, request, id):
-        #print(self.set_contact(id))
-        return HttpResponse(self.set_contact_edit(id))
+
+        return HttpResponse(self.set_contact_edit_view(id))
 
     def post(self, request):
 
@@ -456,3 +481,5 @@ class EditContact(View):
             return HttpResponse("Powrot")
 
 # jak lepije zapisac numer tel + id ownera,
+
+
