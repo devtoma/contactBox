6e1bbf9a5715d49ec357418f24fa7a877b8e56c7
Tomasz Wroblewski <wroblewskitoma@gmail.com>
2019-02-04 00:09:29 +0100

    modification models

--- addressBook/models.py
+++ addressBook/models.py
@@ -14,29 +14,38 @@ class Person(models.Model):
     name = models.CharField(max_length=32)
     surname = models.CharField(max_length=42, default='')
     description = models.TextField(default='')
+    address = models.ForeignKey('Address', on_delete=models.CASCADE, null=True)
+    phones = models.ForeignKey('PhoneNumber', on_delete=models.CASCADE, null=True)
+    emails = models.ForeignKey('EmailAddress', on_delete=models.CASCADE, null=True)
+    groups = models.ManyToManyField('Group')
 
 
 class Address(models.Model):
-    city = models.CharField(max_length=24)
-    street = models.CharField(max_length=32)
-    house_no = models.SmallIntegerField()  # na później: rozstrzygnąć kwestię numerów np. 15c
-    flat_no = models.SmallIntegerField(null=True)
-    resident = models.ForeignKey(Person, on_delete=models.DO_NOTHING)
+    city = models.CharField(max_length=24, default='')
+    street = models.CharField(max_length=32, default='')
+    house_no = models.CharField(max_length=5, default='')  # na później: rozstrzygnąć kwestię numerów np. 15c
+    # flat_no = models.CharField(max_length=5, default='')
+
 
 
 class PhoneNumber(models.Model):
 
-    number = models.CharField(max_length=15)
-    category = models.SmallIntegerField(choices=CATEGORIES)
-    owner = models.ForeignKey(Person, on_delete=models.DO_NOTHING)
+    number = models.CharField(max_length=20, default='')
+    category = models.SmallIntegerField(choices=CATEGORIES, default=1)
 
+
+    def __str__(self):
+        return self.number
 
 class EmailAddress(models.Model):
-    address = models.CharField(max_length=32)
-    category = models.SmallIntegerField(choices=CATEGORIES)
-    owner = models.ForeignKey(Person, on_delete=models.DO_NOTHING)
+    address = models.CharField(max_length=32, default='')
+    category = models.SmallIntegerField(choices=CATEGORIES, default=1)
+
 
 
 class Group(models.Model):
-    name = models.CharField(max_length=15)
-    member = models.ManyToManyField(Person)
+    name = models.CharField(max_length=16)
+
+
+    def __str__(self):
+        return self.name
